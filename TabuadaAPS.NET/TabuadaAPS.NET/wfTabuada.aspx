5adc8eff09af705eefcaefc417c98182a3c55273
Jadenir Souza <jadenir.p.s@gmail.com>
2019-09-14 15:44:29 -0300

    Commit tabuada

---
+++ TabuadaAPS.NET/TabuadaAPS.NET/wfTabuada.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wfTabuada.aspx.cs" Inherits="TabuadaAPS.NET.wfTabuada" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Tabuada</h1>
+            <asp:DropDownList ID="DDLNUMEROS" runat="server" Width="125px">
+            </asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="DDLOPERACAO" runat="server" Width="125px">
+                <asp:ListItem Selected="True">Adição</asp:ListItem>
+                <asp:ListItem>Subtração</asp:ListItem>
+                <asp:ListItem>Multiplicação</asp:ListItem>
+                <asp:ListItem>Divisão</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="BTNEXECUTAR" runat="server" Width="125px" Text="Exibir tabuada" OnClick="BTNEXECUTAR_Click" />
+        </div>
+        <asp:ListBox ID="LBRESULTADO" runat="server" Height="209px" Width="125px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ TabuadaAPS.NET/TabuadaAPS.NET/wfTabuada.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TabuadaAPS.NET
+{
+    public partial class wfTabuada : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Valida se está chamando pela primeira vez
+            if (! IsPostBack)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    DDLNUMEROS.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        protected void BTNEXECUTAR_Click(object sender, EventArgs e)
+        {
+            //limpa os dados da ListBox
+            LBRESULTADO.Items.Clear();
+
+            ListItem li = DDLNUMEROS.SelectedItem;
+            int n = Convert.ToInt32(li.Value);
+            int t = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (DDLOPERACAO.SelectedValue == "Adição")
+                {
+                    t = i + n;
+                    LBRESULTADO.Items.Add(i.ToString() + " + " + n.ToString() + " = " + t.ToString());
+                }
+                else if (DDLOPERACAO.SelectedValue == "Subtração")
+                {
+                    t = i - n;
+                    LBRESULTADO.Items.Add(i.ToString() + " - " + n.ToString() + " = " + t.ToString());
+                }
+                else if (DDLOPERACAO.SelectedValue == "Multiplicação")
+                {
+                    t = i * n;
+                    LBRESULTADO.Items.Add(i.ToString() + " X " + n.ToString() + " = " + t.ToString());
+                }
+                else
+                {
+                    t = i / n;
+                    LBRESULTADO.Items.Add(i.ToString() + " / " + n.ToString() + " = " + t.ToString());
+                }
+            }
+        }
+    }
+}
